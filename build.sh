888fe00d698f913e308d8e440bcadc1b531bf4fe
Hesham Abd-Elmegid <hmegid@instabug.com>
2019-04-21 14:33:37 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 job_image_name="${ANKA_IMAGE_NAME}-${BUILDKITE_JOB_ID}"
-artifacts_directory="/data"
+# artifacts_directory="/data"
 # whoami
 
 # sudo mkdir $artifacts_directory
@@ -18,9 +18,10 @@ anka clone "$ANKA_IMAGE_NAME" "$job_image_name"
 trap cleanup EXIT
 
 echo "+++ Building app in $job_image_name"
-anka run "$job_image_name" pwd
-anka run "$job_image_name" ls -laht /private/var/tmp/ankafs.0/
-anka run "$job_image_name" swift build -v --build-path "$artifacts_directory"
+# anka run "$job_image_name" pwd
+# anka run "$job_image_name" ls -laht /private/var/tmp/ankafs.0/
+anka run -v $PWD:/Users/anka "$job_image_name" swift build
+# -v --build-path "$artifacts_directory"
 
 echo "--- Uploading build artifacts"
-buildkite-agent artifact upload "$artifacts_directory"
+# buildkite-agent artifact upload "$artifacts_directory"
