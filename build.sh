aef22c29c4629d608004cce5a5f8a8173691fb9f
Hesham Abd-Elmegid <hmegid@instabug.com>
2019-04-23 13:45:20 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,8 +21,10 @@ echo "+++ Building app in $job_image_name"
 # anka run "$job_image_name" pwd
 # anka run "$job_image_name" ls -laht /private/var/tmp/ankafs.0/
 # anka run -v $PWD:/Users/anka/workspace "$job_image_name" swift build
-anka run "$job_image_name" swift build
-# anka run "$job_image_name" mkdir temp-bk
+
+anka run "$job_image_name" mkdir build-artifacts
+anka run "$job_image_name" swift build --build-path build-artifacts
+
 # anka run "$job_image_name" ls
 # -v --build-path "$artifacts_directory"
 
